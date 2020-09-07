5ecb4f472937fe7953d2a4655339b1e616eca4c7
Ch0000 <cpan0000@gmail.com>
2020-09-07 12:48:12 -0400

    edit

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+class Dog
+end
+
+Deejay = Dog.new
+Daisy = Dog.new
+Data = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+end
+
+Joey Abate = Person.new
+Mickey Carrol = Person.new
+Mckenna Del Rossi = Person.new
